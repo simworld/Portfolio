487a7892cd81218acc158f1c5eba849699dbb0eb
Simone Susino <simworld@tuta.io>
2024-10-18 23:03:58 +0100

    fixing links

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -26,19 +26,19 @@ document.addEventListener("DOMContentLoaded", function () {
         switch (index) {
           case 0:
             newLink.textContent = "About";
-            newLink.href = "../html/about.html";
+            newLink.href = "/portfolio/html/about.html";
             break;
           case 1:
             newLink.textContent = "Projects";
-            newLink.href = "../html/projects.html";
+            newLink.href = "/portfolio/html/projects.html";
             break;
           case 2:
             newLink.textContent = "Contact";
-            newLink.href = "../html/contact.html";
+            newLink.href = "/portfolio/html/contact.html";
             break;
           case 3:
             newLink.textContent = "Blog";
-            newLink.href = "../html/blog.html";
+            newLink.href = "/portfolio/html/blog.html";
             break;
           default:
             break;
